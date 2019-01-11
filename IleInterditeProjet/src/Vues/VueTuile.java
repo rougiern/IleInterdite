36aeb4860ef49435e00641e21ea3448593739b7f
duttod <45597973+duttod@users.noreply.github.com>
2019-01-11 11:51:32 +0100

    Add files via upload

--- IleInterditeProjet/src/Vues/VueTuile.java
+++ IleInterditeProjet/src/Vues/VueTuile.java
@@ -6,9 +6,12 @@ import ileinterditeproj.Tuile;
 import ileinterditeproj.Utils;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import javax.swing.AbstractButton;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class VueTuile extends JPanel {
     
@@ -18,12 +21,20 @@ public class VueTuile extends JPanel {
     public VueTuile(Tuile t){
         super(new BorderLayout());
         this.tuile=t;
-        bouton = new JButton(getTuile().getNom());
+        if(tuile.getTresor()!=null){
+                bouton = new JButton("<HTML><BODY>"+tuile.getNom()+"<BR>"+"Trésor:"+tuile.getTresor().toString()+"</BODY></HTML>");
+                bouton.setVerticalTextPosition(AbstractButton.CENTER);
+        }else{
+                bouton = new JButton(tuile.getNom());
+        }
+        
         this.add(bouton,BorderLayout.CENTER);
         if(tuile.getEtat()==Utils.EtatTuile.ASSECHEE){
             bouton.setBackground(Parameters.TUILE_ASSECHEE_BG);
+   
         }else if(tuile.getEtat()==Utils.EtatTuile.INONDEE){
             bouton.setBackground(Parameters.TUILE_INONDEE_BG);
+           
         }else if(tuile.getEtat()==Utils.EtatTuile.COULEE){
            
         }
