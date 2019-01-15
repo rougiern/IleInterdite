edc92ff13a6a013a0ac4550292f386bfb46b02c9
kemplail <45623703+kemplail@users.noreply.github.com>
2019-01-15 01:07:11 +0100

    Add files via upload

--- IleInterditeProjet/src/Vues/VueCarteSpeciale.java
+++ IleInterditeProjet/src/Vues/VueCarteSpeciale.java
@@ -32,87 +32,105 @@ import ileinterditeproj.CarteMonteedesEaux;
 import ileinterditeproj.CarteHelicoptere;
 import ileinterditeproj.CarteTresor;
 import ileinterditeproj.Utils;
-import java.util.ArrayList;        
+import java.util.ArrayList;
+
 /**
  *
  * @author davidpa
  */
 public class VueCarteSpeciale extends Observable {
-        private final JFrame window;
-        private final JButton btnCartesacDesable;
-        private final JButton btnCarteHelico;
-        private JPanel mainPanel;
-        //private final
-    public VueCarteSpeciale(Aventurier a){
+
+    private final JFrame window;
+    private final JButton btnCartesacDesable;
+    private final JButton btnCarteHelico;
+    private JPanel mainPanel;
+    //private final
+
+    public VueCarteSpeciale(Aventurier a) {
 
         this.window = new JFrame();
-        window.setSize(500,200);
+        window.setSize(500, 200);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        window.setLocation(dim.width/2-window.getSize().width/2, dim.height/2-window.getSize().height/2);
+        window.setLocation(dim.width / 2 - window.getSize().width / 2, dim.height / 2 - window.getSize().height / 2);
         //le titre = nom du joueur 
         window.setTitle("Vos cartes speciales");
-        mainPanel = new JPanel(new GridLayout(1,2));
+        mainPanel = new JPanel(new GridLayout(1, 2));
         window.add(mainPanel);
-                int nbcarteSacDeSable = 0;
-                int nbcarteHelicoptere = 0;
-                if (!a.getMains().isEmpty() ){
-                    for (CarteTirage carteTirage : a.getMains())
-                         if (carteTirage instanceof CarteSacDeSable){
-                             nbcarteSacDeSable++;
-                         }
-
-                        else if (carteTirage instanceof CarteHelicoptere){
-                             nbcarteHelicoptere++;
-                         }
+        
+        int nbcarteSac = 0;
+        int nbcarteHelico = 0;
+        
+        if (!a.getMains().isEmpty()) {
+            for (CarteTirage carteTirage : a.getMains()) {
+                if (carteTirage instanceof CarteSacDeSable) {
+                    nbcarteSac++;
+                } else if (carteTirage instanceof CarteHelicoptere) {
+                    nbcarteHelico++;
                 }
-                
-        btnCartesacDesable = new JButton("Carte sac de sable("+nbcarteSacDeSable+")" );
+            }
+        }
+
+        btnCartesacDesable = new JButton("Carte sac de sable(" + nbcarteSac + ")");
         btnCartesacDesable.setBackground(a.getPion().getCouleur());
         mainPanel.add(btnCartesacDesable);
-        if(nbcarteSacDeSable ==0){
+        if (nbcarteSac == 0) {
             btnCartesacDesable.setEnabled(false);
         }
         btnCartesacDesable.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 setChanged();
-                notifyObservers(new Message(Commandes.ASSECHER,a.getNom()));
+                notifyObservers(new Message(Commandes.ASSECHER, a.getNom()));
                 clearChanged();
             }
         });
-        
-        
-        
-        btnCarteHelico = new JButton("Carte Helicoptère("+nbcarteSacDeSable+")" );
+
+        btnCarteHelico = new JButton("Carte Helicoptère(" + nbcarteHelico + ")");
         btnCarteHelico.setBackground(a.getPion().getCouleur());
         mainPanel.add(btnCarteHelico);
-        if(nbcarteHelicoptere == 0){
+        if (nbcarteHelico == 0) {
             btnCarteHelico.setEnabled(false);
-        } 
+        }
         btnCarteHelico.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 setChanged();
-                notifyObservers(new Message(Utils.Commandes.UTILISER_HELICO,a.getNom()));
+                notifyObservers(new Message(Utils.Commandes.UTILISER_HELICO, a.getNom()));
                 clearChanged();
             }
         });
-        
-        
-        
+
         window.setVisible(true);
     }
-    
-    
-    public void actualiserPourDeplacer(ArrayList<Aventurier> joueurs){
-        window.setSize(350,500);
-        window.setVisible(false);
+
+    public void actualiserPourDeplacer(ArrayList<Aventurier> joueurs) {
         mainPanel.removeAll();
-        mainPanel.setLayout(new GridLayout(joueurs.size(),2));
-        for(Aventurier avt : joueurs ){
-            mainPanel.add(new JLabel(avt.getNom()));
-            mainPanel.add(new JButton("Déplacer"));
+        mainPanel.setLayout(new GridLayout(joueurs.size(), 2));
+        JButton boutondeplacer;
+        for (Aventurier avt : joueurs) {
+            boutondeplacer = new JButton("Déplacer");
+            
+            mainPanel.add(new JLabel(avt.getNom(), SwingConstants.CENTER));
+            mainPanel.add(boutondeplacer);
+            
+            boutondeplacer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setChanged();
+                notifyObservers(new Message(Utils.Commandes.BOUGER_HELICO, avt.getNom()));
+                clearChanged();
+            }
+        });
         }
         mainPanel.revalidate();
     }
+
+    public void close() {
+        this.window.dispose();
+    }
+
+    public void afficher() {
+        this.window.setVisible(true);
+    }
+
 }
